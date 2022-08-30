a0d07434d924f51251edd005395a5fd34cbac260
AzamFashionValet <azam@fashionvalet.com>
2022-08-31 02:04:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/ui/TomorrowFragment.kt
@@ -0,0 +1,22 @@
+package com.example.weatherapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.databinding.FragmentTomorrowBinding
+
+class TomorrowFragment : Fragment() {
+    private var _binding: FragmentTomorrowBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentTomorrowBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
